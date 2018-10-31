c03c1d5e7551e9a88666510637e33934c83b2aeb
Stephen Dixon <coding@btinternet.com>
2018-10-31 13:46:11 +0000

    Modified to use functions

--- directories.py
+++ directories.py
@@ -11,7 +11,54 @@ Created on Tue Oct 30 15:48:06 2018
 import os, shutil, time
 from astropy.io import fits
 
-# create directory structure
+# function definitions
+
+def createWorkingDirectories(directories):
+    for dir in directories:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+def copyFiles(source_dir,dest_dir):
+    for file in os.listdir(source_dir):
+        src_file = os.path.join(source_dir,file)
+        shutil.copy(src_file,dest_dir)
+
+def moveImageType(source_dir,image_type,dest_dir):
+    for file in os.listdir(source_dir):
+        src_file = os.path.join(source_dir,file)
+        with fits.open(src_file) as hdul:
+            hdr = hdul[0].header
+            try:
+                image = hdr['IMAGETYP']
+                if image == image_type:
+                    try:
+                        shutil.move(src_file,dest_dir)
+                    except shutil.Error:
+                        print("file error")
+                        os.remove(src_file)    
+            except KeyError:
+                print("No IMAGETYP in Header")
+
+def splitFilters(source_dir):
+    for file in os.listdir(source_dir):
+        src_file = os.path.join(source_dir,file)
+        if os.path.isfile(src_file):
+            with fits.open(src_file) as hdul:
+                hdr = hdul[0].header
+                try:
+                    filtertype = hdr['FILTER']
+                    dest_dir = os.path.join(source_dir,filtertype)
+                    if not os.path.exists(dest_dir):
+                        os.makedirs(dest_dir)
+                    try:
+                        shutil.move(src_file,dest_dir)
+                    except shutil.Error:
+                        print("file error")
+                        os.remove(src_file)
+                except KeyError:
+                    print("No FILTER in Header")
+
+
 
 source_dir = 'samples'
 input_dir = 'working/input'
@@ -22,91 +69,23 @@ master_dir = 'working/calibration/masters'
 science_dir = 'working/science'
 output_dir = 'working/output'
 
-if not os.path.exists(input_dir):
-    os.makedirs(input_dir)
-if not os.path.exists(bias_dir):
-    os.makedirs(bias_dir)
-if not os.path.exists(dark_dir):
-    os.makedirs(dark_dir)
-if not os.path.exists(flat_dir):
-    os.makedirs(flat_dir)
-if not os.path.exists(master_dir):
-    os.makedirs(master_dir)
-if not os.path.exists(science_dir):
-    os.makedirs(science_dir)
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+directoryList = (input_dir,bias_dir,dark_dir,flat_dir,master_dir,science_dir,output_dir)
 
-# Copy file from source directory to input_dir 
+createWorkingDirectories(directoryList)
 
-# commented out for testing need to reinstate    
-#for file in os.listdir(source_dir):
-#    src_file = os.path.join(source_dir,file)
-#    shutil.copy(src_file,input_dir)
-
-# Move Files to correct subdirectories
-# IMAGETYP text indicates file use
-# if no IMAGETYP in header leave in input_dir
+copyFiles(source_dir,input_dir)
     
 image_bias = 'Bias Frame'
 image_dark = 'Dark Frame'
 image_flat = 'Flat Field'
 image_science = 'Light Frame'
 
-for file in os.listdir(input_dir):
-    src_file = os.path.join(input_dir,file)
-    with fits.open(src_file) as hdul:
-        hdul.info()
-        hdr = hdul[0].header
-        try:
-            image = hdr['IMAGETYP']
-            #print(repr(image))
-            if image == image_bias:
-                try:
-                    shutil.move(src_file,bias_dir)
-                except shutil.Error:
-                    print("file error")
-                    os.remove(src_file)
-            elif image == image_dark:
-                try:
-                    shutil.move(src_file,dark_dir)
-                except shutil.Error:
-                    print("file error")
-                    os.remove(src_file)
-            elif image == image_flat:
-                try:
-                    shutil.move(src_file,flat_dir)
-                except shutil.Error:
-                    print("file error")
-                    os.remove(src_file)
-            elif image == image_science:
-                try:
-                    shutil.move(src_file,science_dir)
-                except shutil.Error:
-                    print("file error")
-                    os.remove(src_file)
-        except KeyError:
-            print("No IMAGETYP in Header")
+moveImageType(input_dir,image_bias,bias_dir)
+moveImageType(input_dir,image_dark,dark_dir)
+moveImageType(input_dir,image_flat,flat_dir)
+moveImageType(input_dir,image_science,science_dir)
 
-# Split flats by filter type
-# given by FILTER in header
+splitFilters(flat_dir)
 
-for file in os.listdir(flat_dir):
-    src_file = os.path.join(flat_dir,file)
-    with fits.open(src_file) as hdul:
-        hdul.info()
-        hdr = hdul[0].header
-        try:
-            filtertype = hdr['FILTER']
-            print(repr(filtertype))
-            dest_dir = os.path.join(flat_dir,filtertype)
-            if not os.path.exists(dest_dir):
-                os.makedirs(dest_dir)
-            try:
-                shutil.move(src_file,dest_dir)
-            except shutil.Error:
-                print("file error")
-                os.remove(src_file)
-        except KeyError:
-            print("No FILTER in Header")
+
     
