d9fc98d7c7633e4ddd7bbf7b209b7d55a440f70d
Stephen Dixon <coding@btinternet.com>
2018-12-23 15:30:50 +0000

    GUI Dev 2

--- GUI_Dev.py
+++ GUI_Dev.py
@@ -13,9 +13,14 @@ import configparser
 import io
 
 def newFile():
-    #code here
-    test == 1
-
+    
+    n = ttk.Notebook(win)
+    n.pack()
+    f1 = ttk.Frame(n)
+    f2 = ttk.Frame(n)
+    n.add(f1, text='One')
+    n.add(f2, text='Two')
+    
 def openFile():
    
     # Load the configuration file
@@ -63,37 +68,35 @@ def aboutProg():
     test == 7
     
 def _init_menu_file(bar,menu):
-    bar.add_cascade(menu=menu_file, label='File', underline=0)
+    bar.add_cascade(menu=menu, label='File', underline=0, accelerator='Control-F')
     
-    menu.add_command(label='New', command=newFile, underline=0)
-    menu.add_command(label='Open...', command=openFile, underline=0)
-    menu.add_command(label='Close', command=closeFile, underline=0)
+    menu.add_command(label='New', command=newFile, underline=0, accelerator='Control-F')
+    menu.add_command(label='Open...', command=openFile, underline=0, accelerator='Control-O')
+    menu.add_command(label='Close', command=closeFile, underline=0, accelerator='Control-C')
     menu.add_separator()
-    menu.add_command(label='Save', command=saveFile, underline=0)
-    menu.add_command(label='Save as...', command=saveasFile, underline=5)
+    menu.add_command(label='Save', command=saveFile, underline=0, accelerator='Control-S')
+    menu.add_command(label='Save as...', command=saveasFile, underline=5, accelerator='Control+Shift+S')
     menu.add_separator()
-    menu.add_command(label='Quit', command=quitProg, underline=0)
+    menu.add_command(label='Quit', command=quitProg, underline=0, accelerator='Control-Q')
 
 def _init_menu_edit(bar,menu):
-    bar.add_cascade(menu=menu_edit, label='Edit', underline=0)
-    
+    bar.add_cascade(menu=menu, label='Edit', underline=0, accelerator='Control-E')
 
 def _init_menu_help(bar,menu):
-    bar.add_cascade(menu=menu_help, label='Help', underline=0)
+    bar.add_cascade(menu=menu, label='Help', underline=0, accelerator='Control-H')
     
     menu.add_separator()
-    menu.add_command(label='About..', command=aboutProg, underline=0)
+    menu.add_command(label='About..', command=aboutProg, underline=0, accelerator='Control-A')
     
 def _init_menu():
     menubar = Menu(win)
     menu_file = Menu(menubar)
     menu_edit = Menu(menubar)
     menu_help = Menu(menubar)
-    
+
     _init_menu_file(menubar,menu_file)
     _init_menu_edit(menubar,menu_edit)
     _init_menu_help(menubar,menu_help)
-
 
     win['menu'] = menubar    
     
@@ -105,6 +108,14 @@ root.withdraw()
 win = Toplevel(root)
 win.protocol("WM_DELETE_WINDOW", root.destroy)
 win.title(' MHT CCD Pipeline ')
+win.geometry('300x200-5+40')
+
+#n = ttk.Notebook(win)
+#f1 = ttk.Frame(n)
+#f2 = ttk.Frame(n)
+#n.add(f1, text='One')
+#n.add(f2, text='Two')
+#n.pack()
 
 #but = Button(win, text='deiconify')
 #but['command'] = root.deiconify
