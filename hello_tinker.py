8363b3305920090ddb0564ea03b133ba9e638849
Stephen Dixon <coding@btinternet.com>
2018-12-26 19:22:58 +0000

    GUI Dev 3

---
+++ hello_tinker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 23 15:36:11 2018
+
+@author: sdixon
+"""
+
+"""Hello World application for Tinker"""
+
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+label = Label(root, text="Hello World")
+label.pack()
+
+root.mainloop()
